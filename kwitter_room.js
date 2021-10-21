f7284bbe13b7258b8e1bd47f239835b07f9c0e02
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2021-10-20 17:58:09 -0700

    Add files via upload
    ok so some things arent defined yet but will do that at a later date

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+  var firebaseConfig = {
+      apiKey: "AIzaSyC9febsNBOnHXCh-wc7w-n9pEyzIkxyTjI",
+      authDomain: "kwitter-d585a.firebaseapp.com",
+      databaseURL: "https://kwitter-d585a-default-rtdb.firebaseio.com",
+      projectId: "kwitter-d585a",
+      storageBucket: "kwitter-d585a.appspot.com",
+      messagingSenderId: "741452678462",
+      appId: "1:741452678462:web:fe9a5b8d9e26ed7af9586f",
+      measurementId: "G-CR64FNWZVE"
+    };
+      firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
